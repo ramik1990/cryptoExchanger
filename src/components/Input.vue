0d2c1a6785bee54c73fd63d6b373aaed750d9ac8
ramik1990 <ramikshudabaev@gmail.com>
2024-02-26 23:35:19 +0600

    Feat: first steps

---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+<input type="number">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+input {
+    outline: none;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+}
+</style>
