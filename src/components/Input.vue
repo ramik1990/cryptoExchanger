5e231384f8783a160ad907e62741025f73efefa4
ramik1990 <ramikshudabaev@gmail.com>
2024-02-27 00:06:00 +0600

    feat: complete main design

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,24 +1,25 @@
 <template>
-<input type="number" placeholder="Введите число">
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style scoped>
-input {
-    width: 500px;
-    position: relative;
-    top: -50px;
-    font-size: 1.2em;
-    outline: none;
-    border-radius: 3px;
-    border: 0;
-    padding: 10px 15px;
-    background: #fafafa;
-    color: #333;
-}
-</style>
+    <input type="number" placeholder="Введите число">
+    </template>
+    
+    <script>
+    export default {
+    
+    }
+    </script>
+    
+    <style scoped>
+    input {
+        width: 500px;
+        position: relative;
+        top: -50px;
+        font-size: 1.2em;
+        outline: none;
+        border-radius: 3px;
+        border: 0;
+        padding: 10px 15px;
+        background: #fafafa;
+        color: #333;
+    }
+    </style>
+    
